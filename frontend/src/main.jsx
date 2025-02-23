898ded914bfb74eedc20b0729db26147a69e35dc
gerusalobo <gerusalobo@gmail.com>
2025-02-23 17:12:37 -0300

    queryprovider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,6 +11,9 @@ import AboutPage from './routes/AboutPage.jsx'
 import Write from './routes/Write.jsx'
 import MainLayout from './layouts/MainLayout.jsx'
 import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 // Import Clerk Public Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -58,7 +61,9 @@ const router = createBrowserRouter ([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+      </QueryClientProvider>        
     </ClerkProvider>
   </StrictMode>,
 )
