29de8651032e4544c11b9ba548068639bce3ae2e
gerusalobo <gerusalobo@gmail.com>
2025-02-24 08:52:58 -0300

    fix

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,12 +1,10 @@
 import { IKImage } from "imagekitio-react";
 
 const Image = ({ src, className, w, h, alt }) => {
-  const validSrc = src && src.trim() !== "" ? src : null;
-
-  return validSrc ? (
+  return (
     <IKImage
       urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT}
-      path={validSrc}
+      path={src}
       className={className}
       loading="lazy"
       lqip={{ active: true, quality: 20 }}
@@ -20,7 +18,7 @@ const Image = ({ src, className, w, h, alt }) => {
         },
       ]}
     />
-  ) : null; // Retorna `null` se não houver imagem válida
+  );
 };
 
 export default Image;
